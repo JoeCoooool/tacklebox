28760d3c11c77cd3b2fd586e3c3b87452ce972b0
JoeCoool <59798357+JoeCoooool@users.noreply.github.com>
2026-01-04 17:16:29 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 # TACKLEBOX LITE-INSTALLER
-
 set -e
 echo "🎣 Starte TackleBox LXC Installation..."
 
-# Container ID finden
-CT_ID=$(pvesh get /cluster/nextid)
+# Container ID finden und Leerzeichen/Umbrüche entfernen
+CT_ID=$(pvesh get /cluster/nextid | tr -d '\r' | tr -d ' ')
 
-# LXC Container erstellen (Alles in einer Zeile!)
 echo "Erstelle Container $CT_ID..."
+# Befehl in einer einzigen langen Zeile, um Backslash-Fehler zu vermeiden
 pct create $CT_ID local:vztmpl/debian-12-standard_12.0-1_amd64.tar.zst --hostname tacklebox --password tacklebox123 --net0 name=eth0,bridge=vmbr0,ip=dhcp --rootfs local-lvm:4 --memory 512 --unprivileged 1
 
-# Starten
+echo "Starte Container..."
 pct start $CT_ID
-echo "Warte auf Netzwerk..."
 sleep 10
 
-# Software installieren
-echo "Installiere Apache & PHP..."
+echo "Installiere Software..."
 pct exec $CT_ID -- apt-get update
 pct exec $CT_ID -- apt-get install -y apache2 php php-sqlite3 php-gd php-curl php-xml curl
 
-# Dateien laden
-echo "Lade index.php von GitHub..."
+echo "Lade index.php..."
 pct exec $CT_ID -- rm /var/www/html/index.html
 pct exec $CT_ID -- curl -sL -o /var/www/html/index.php https://raw.githubusercontent.com/JoeCoooool/tacklebox/main/index.php
 
-# Rechte
+echo "Setze Rechte..."
 pct exec $CT_ID -- chown -R www-data:www-data /var/www/html/
 pct exec $CT_ID -- chmod -R 755 /var/www/html/
 
-IP=$(pct exec $CT_ID -- hostname -I | awk '{print $1}')
+IP=$(pct exec $CT_ID -- hostname -I | awk '{print $1}' | tr -d '\r')
 echo "---------------------------------------------------"
 echo "✅ FERTIG! URL: http://$IP"
 echo "---------------------------------------------------"
